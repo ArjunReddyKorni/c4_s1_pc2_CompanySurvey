31b7ce9d26060cbf0d1e34a03d135e83d48f3713
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-23 17:05:15 +0530

    Initial commit

---
+++ src/base/AverageAge.java
@@ -0,0 +1,31 @@
+ package base;
+public class AverageAge {
+
+    public static void main(String[] args) {
+
+        String[] age = {"35","26","32","44"};
+
+        AverageAge averageAge = new AverageAge();
+
+         String average = averageAge.averageAgeCalculator(age);
+
+        System.out.println("average = " + average);
+
+    }
+
+    public String averageAgeCalculator(String[] ageRawData){
+        double average = 0;
+        int total = 0;
+        try{
+            for (int i = 0; i < ageRawData.length-1 ; i++) {
+                 total =  total + Integer.parseInt(ageRawData[i]) ;
+                average = total/ageRawData.length;
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("e = " + e);
+        }
+        String result = String.valueOf(average);
+        return result;
+    }
+}
